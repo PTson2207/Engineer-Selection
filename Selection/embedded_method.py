c30d8041be9b3ab2210979a7c93eeac88470b90d
Master-Snack <sonphukl@gmail.com>
2021-05-14 19:28:47 +0700

    update new feature

--- Selection/embedded_method.py
+++ Selection/embedded_method.py
@@ -15,7 +15,7 @@ def rf_importance(X_train, y_train, max_depth=10, class_weight=None, top_n=15, n
     std = np.std([tree.feature_importances_ for tree in model.estimators_], axis=0)
     print("Xếp hạng các feature:")
     for f in range(X_train.shape[1]):
-        print("%d. feature no: %d. feature name: %s (%f)" % (f+1, indices[f], feat_labels[indices[f]], importances[indices[f]]))
+        print("%d. feature num: %d. feature name: %s (%f)" % (f+1, indices[f], feat_labels[indices[f]], importances[indices[f]]))
 
     # visualize
     indices = indices[0:top_n]
@@ -29,7 +29,7 @@ def rf_importance(X_train, y_train, max_depth=10, class_weight=None, top_n=15, n
     return model  
 
 def dt_importance(X_train, y_train, max_depth=10, top_n=20, random_state=0):
-    model = DecisionTreeRegressor(max_depth=max_depth, n_jobs=-1, random_state=random_state)
+    model = DecisionTreeRegressor(max_depth=max_depth, random_state=random_state)
     model.fit(X_train, y_train)
     importances = model.feature_importances_
     indices = np.argsort(importances)[::-1]
@@ -37,7 +37,7 @@ def dt_importance(X_train, y_train, max_depth=10, top_n=20, random_state=0):
     std = np.std([tree.feature_importances_ for tree in model.estimators_], axis=0)
     print("Xếp hạng các feature:")
     for f in range(X_train.shape[1]):
-        print("%d. feature no: %d. feature name: %s (%f)" % (f+1, indices[f], feat_labels[indices[f]], importances[indices[f]]))
+        print("%d. feature num: %d. feature name: %s (%f)" % (f+1, indices[f], feat_labels[indices[f]], importances[indices[f]]))
 
     # visualize
     indices = indices[0:top_n]
